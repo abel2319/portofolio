d82d9221841553e77b8e3057f18791a56eb12509
abel2319 <kpohinto.abel@gmail.com>
2024-09-30 13:35:11 +0200

    done with 60% of expe

--- src/components/experiences/experience.jsx
+++ src/components/experiences/experience.jsx
@@ -6,24 +6,48 @@ function Experience() {
     <div className='expe'>
       <p>Expérience</p>
       <div className="expeItems">
-        <div className="item">
+        <div className="expeItem">
           <div className="year">
-            Mars 2024– Août 2024
+            Mars - Août 2024 (6 mois)
           </div>
           <div className="dotline">
             <div className="dot"></div>
             <div className="line"></div>
           </div>
-          <div className="content">
-            <h2>Analyste Programmeur (Stage)</h2> 
-              Institut de Mathématiques et de Sciences Physiques (IMSP) – Dangbo, Benin – (6 mois) <br />
+          <div className="expeContent">
+            <span className='expeItemTitle'>Analyste Programmeur (Stage)</span>
+            <p>
+            <em>Institut de Mathématiques et de Sciences Physiques (IMSP) – Dangbo, Benin</em> <br />
 
-              • Assistance aux utilisateurs pour la prise en main du supercalculateur de l’IMSP <br />
-              • Assistance aux utilisateurs pour l’exécution des modèles de machine learning sur le supercalculateur <br />
-              • Mise en place d’un site web de création de mails institutionnels aux etudiants <br />
-              • IT support au sein de l’institut (dépannage du réseaux informatique et aide à la prise en main des logiciels) <br />
-              • Assistant formateur à la formation d’initiation au calcul haute performance <br />
-              • <span> Technologies </span>: <em> Python, C++, Slurm, Git, LaTeX, JavaScript</em> <br />
+            • Assistance aux utilisateurs pour la prise en main du supercalculateur de l’IMSP l’exécution des modèles de machine learning. <br />
+            • Assistance aux utilisateurs pour l’exécution des modèles de machine learning sur le supercalculateur <br />
+            • Mise en place d’un site web de création de mails institutionnels aux etudiants <br />
+            • IT support au sein de l’institut (dépannage du réseaux informatique et aide à la prise en main des logiciels) <br />
+            • Assistant formateur à la formation d’initiation au calcul haute performance <br />
+            • <strong> Technologies </strong>: <em> Python, C++, Slurm, Git, LaTeX, JavaScript</em> <br />
+            </p>
+          </div>
+        </div>
+        <div className="expeItem">
+          <div className="year">
+            Mars - Août 2024 (6 mois)
+          </div>
+          <div className="dotline">
+            <div className="dot"></div>
+            <div className="line"></div>
+          </div>
+          <div className="expeContent">
+          <span className='expeItemTitle'>Analyste Programmeur (Stage)</span>
+            <p>
+            <em>Institut de Mathématiques et de Sciences Physiques (IMSP) – Dangbo, Benin</em> <br />
+
+            • Assistance aux utilisateurs pour la prise en main du supercalculateur de l’IMSP l’exécution des modèles de machine learning. <br />
+            • Assistance aux utilisateurs pour l’exécution des modèles de machine learning sur le supercalculateur <br />
+            • Mise en place d’un site web de création de mails institutionnels aux etudiants <br />
+            • IT support au sein de l’institut (dépannage du réseaux informatique et aide à la prise en main des logiciels) <br />
+            • Assistant formateur à la formation d’initiation au calcul haute performance <br />
+            • <strong> Technologies </strong>: <em> Python, C++, Slurm, Git, LaTeX, JavaScript</em> <br />
+            </p>
           </div>
         </div>
       </div>
